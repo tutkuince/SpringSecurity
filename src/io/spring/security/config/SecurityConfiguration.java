1cdb4ed58c5cc8d0fab50d82c9e8bff644831208
tutkuince <tutku.ince@outlook.com>
2018-08-06 19:02:06 +0300

    comment to configure(AuthenticationManagerBuilder) method for creating our custom login form

--- src/io/spring/security/config/SecurityConfiguration.java
+++ src/io/spring/security/config/SecurityConfiguration.java
@@ -1,30 +1,25 @@
 package io.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentication
-
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-		UserDetails user1 = User.withUsername("ugur").password(encoder.encode("111.")).roles("EMPLOYEE").build();
-		UserDetails user2 = User.withUsername("tutku").password(encoder.encode("123.")).roles("MANAGER").build();
-		UserDetails user3 = User.withUsername("emin").password(encoder.encode("000.")).roles("ADMIN").build();
-
-		auth.inMemoryAuthentication().withUser(user1).withUser(user2).withUser(user3);
-
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		// add our users for in memory authentication
+//
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//
+//		UserDetails user1 = User.withUsername("ugur").password(encoder.encode("111.")).roles("EMPLOYEE").build();
+//		UserDetails user2 = User.withUsername("tutku").password(encoder.encode("123.")).roles("MANAGER").build();
+//		UserDetails user3 = User.withUsername("emin").password(encoder.encode("000.")).roles("ADMIN").build();
+//
+//		auth.inMemoryAuthentication().withUser(user1).withUser(user2).withUser(user3);
+//
+//	}
 }
