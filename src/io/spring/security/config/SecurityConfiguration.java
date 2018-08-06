6b810771d98fe4131c95e6a316e52f6251bc84b1
tutkuince <tutku.ince@outlook.com>
2018-08-06 19:09:19 +0300

    override configure(HttpSecurity) method for customizing our login form

--- src/io/spring/security/config/SecurityConfiguration.java
+++ src/io/spring/security/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package io.spring.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,4 +23,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //		auth.inMemoryAuthentication().withUser(user1).withUser(user2).withUser(user3);
 //
 //	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()	// restrict access based on the HttpServletRequest
+			.anyRequest().authenticated()	// Any request to the app must be authenticated (ie logged in)
+			.and()
+			.formLogin()	// We are customizing the form login process
+			.loginPage("/showLoginPage")	// show our custom form at the request mapping "/showLoginPage"
+			.loginProcessingUrl("/authenticateTheUser")	// Login form should post data to this url for processing (check user id and password) (spring create)
+			.permitAll();	// Allow everyone to see login page. No need to be logged in.
+	}
 }
