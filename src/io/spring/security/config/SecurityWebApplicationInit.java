3fda554fd6f28ae32dbb07c2846047c1bf0e3077
tutkuince <tutku.ince@outlook.com>
2018-08-06 18:39:43 +0300

    websecurity initializer

---
+++ src/io/spring/security/config/SecurityWebApplicationInit.java
@@ -0,0 +1,7 @@
+package io.spring.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInit extends AbstractSecurityWebApplicationInitializer {
+
+}
