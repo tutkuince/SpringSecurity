462e4fe67f002f67485fe48477182c21d1f1ddc6
tutkuince <tutku.ince@outlook.com>
2018-08-08 19:49:01 +0300

    deleted redundant imports

--- src/io/spring/security/config/SecurityConfiguration.java
+++ src/io/spring/security/config/SecurityConfiguration.java
@@ -8,10 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
